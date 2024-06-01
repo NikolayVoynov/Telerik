6f8db13e370038b5a022d3c63a5f32e8db800c26
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2024-06-01 15:12:45 +0300

    Add files via upload

---
+++ Alpha 61 QA - Course Preparation/src/AboveТheMainDiagonal.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class AboveТheMainDiagonal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}

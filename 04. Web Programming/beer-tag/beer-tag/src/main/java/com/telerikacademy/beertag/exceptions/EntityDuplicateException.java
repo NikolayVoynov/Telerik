76f88e171f927c828ef93edd90d0bfc0d93cea85
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2024-10-21 10:00:00 +0300

    Delete 04. Web Programming/beer-tag/beer-tag directory

--- 04. Web Programming/beer-tag/beer-tag/src/main/java/com/telerikacademy/beertag/exceptions/EntityDuplicateException.java
+++
@@ -1,8 +0,0 @@
-package com.telerikacademy.beertag.exceptions;
-
-public class EntityDuplicateException extends RuntimeException {
-
-    public EntityDuplicateException(String type, String attribute, String value) {
-        super(String.format("%s with %s %s already exists.", type, attribute, value));
-    }
-}

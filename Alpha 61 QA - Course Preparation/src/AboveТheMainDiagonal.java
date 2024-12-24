4d1c49315eda9aced545a290d9a380efad2fe315
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2024-12-24 13:59:34 +0200

    Delete Alpha 61 QA - Course Preparation directory

--- Alpha 61 QA - Course Preparation/src/AboveТheMainDiagonal.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class AboveТheMainDiagonal {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        
-    }
-}
